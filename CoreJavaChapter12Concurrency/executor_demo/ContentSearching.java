65a12f97c898728785e34dba4f08007151d73081
Tien Nguyen <tiennd@saigontech.edu.vn>
2021-04-05 10:34:54 +0700

    check fork-join framework

---
+++ CoreJavaChapter12Concurrency/executor_demo/ContentSearching.java
@@ -0,0 +1,62 @@
+package executor_demo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ContentSearching {
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)){
+            System.out.println("Input a directory: ");
+            String dir = in.nextLine();
+            System.out.println("Input a keyword to search: ");
+            String keyword = in.nextLine();
+            Set<File> filesToSearch = listFileName(dir);
+            System.out.println("The number of files to search: " + filesToSearch.size());
+            var tasks = new ArrayList<Callable<Long>>(4);
+            for(File file: filesToSearch)
+            {
+                Callable<Long> task = () -> occurrence(file, keyword);
+                tasks.add(task);
+            }
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            List<Future<Long>> results = executorService.invokeAll(tasks);
+            long total = 0;
+            for(Future<Long> result: results)
+                total += result.get();
+            System.out.println("total occurences: " + total);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Long occurrence(File file, String keyword)  {
+        try( var in = new Scanner(file))
+        {
+            long count = 0;
+            while(in.hasNext())
+                if(in.next().equals(keyword))
+                    count++;
+                return count;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0L;
+        }
+    }
+
+    private static Set<File> listFileName(String dirName) throws InterruptedException {
+        Set<File> result = new HashSet<>();
+        File dir = new File(dirName);
+        File[] files = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String filename) {
+                return filename.endsWith(".txt");
+            }
+        });
+        for (File file : files)
+            result.add(file);
+        return  result;
+
+    }
+}
---
+++ CoreJavaChapter12Concurrency/fork_join_demo/ForkJoinTest.java
@@ -0,0 +1,53 @@
+package fork_join_demo;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.function.DoublePredicate;
+
+public class ForkJoinTest {
+    public static void main(String[] args) {
+        final int SIZE = 1000000;
+        var numbers = new double[SIZE];
+        for(int i = 0; i < SIZE; i++)
+            numbers[i] = Math.random();
+        var counter = new Counter(numbers, 0, numbers.length, x -> (x > 0.5));
+        var pool = new ForkJoinPool();
+        pool.invoke(counter);
+        System.out.println(counter.join());
+    }
+
+
+    private static class Counter extends RecursiveTask<Integer> {
+        public static final int THRESH_HOLD = 1000;
+        private double [] values;
+        private int to;
+        private int from;
+        private DoublePredicate filter;
+        public Counter(double[] numbers, int from, int to, DoublePredicate filter) {
+            this.values = numbers;
+            this.from = from;
+            this.to = to;
+            this.filter = filter;
+        }
+
+        @Override
+        protected Integer compute() {
+            if( to - from < THRESH_HOLD) {
+                int count = 0;
+                for (int i = from; i < to; i++)
+                    if(filter.test(values[i]))
+                        count++;
+                return count;
+            }
+            else
+            {
+                int mid = (from + to)/2;
+                var first = new Counter(values, from, mid, filter);
+                var last = new Counter(values, mid, to, filter);
+                invokeAll(first,last);
+                return first.join() + last.join();
+
+        }
+    }
+}
+}
